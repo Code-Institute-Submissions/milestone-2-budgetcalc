7ae218f0fe396ce353c4e26d296e0ee239bab1c1
Alexander Forrest <asforrest@icloud.com>
2020-10-15 21:50:46 +0000

    FUNCTION: Added emailJS to send users the information they fill in in an email.

---
+++ js/sendEmail.js
@@ -0,0 +1,22 @@
+/* The following code has been adapted from the Code Institute lessons on adding emailJS API to a Web App */
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "BudgetCalcTemplate", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "accountBankValue": accountBankValue.value,
+        "accountSavingsValue": accountSavingsValue.value,
+        "totalAccountsValue": totalAccountsValue,
+
+
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
