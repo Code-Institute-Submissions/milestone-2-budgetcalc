c2634887e6c5f1ce966e1a96554ff0fe566c731b
Alexander Forrest <asforrest@icloud.com>
2020-10-15 01:12:22 +0000

    Feature / TEST: Added new calulcaiton method fearure with autofill from totalAccountsValue to totalIncomeValue. It's working.

--- - BACKLOG - primary.md
+++ - BACKLOG - primary.md
@@ -1,9 +1,17 @@
 # BACKLOG primary
 
 ## CURRENT SPRINT
-- remove calculate button and make the results populate an input field
+- [ ] Add auto calculation method to all sections
 
 ## BACKLOG
-- Send email via emailJS to user with budget information
+- [ ] Send email via emailJS to user with budget information
+
+- [ ] Have selction option (probably button) to choose currency and fill that currency in front of the numbers, possibly with a <span id="currency">?
+
+- [ ] Allow person to select which month it is
+
+- [ ] Select the amount of people in the household 1 or 2
+- [ ] Add checkboxes next to elements for joint/shared costs or not
 
 ## DONE
+- [x] remove calculate button and make the results populate an input field
--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,10 @@
-/* REF: Adding two numbers together SRC: https://code-maven.com/slides/javascript/solution-add-two-numbers */
+/* The following code has been adapted from 3 sources:
+the source code of http://www.youcandealwithit.com/borrowers/calculators-and-resources/calculators/budget-calculator.shtml
+the explination on https://stackoverflow.com/questions/30806948/javascript-run-function-on-input-change-no-jquery
+and support from Michael - Code Institute Tutor Support */
 
 "use strict";
-/*
-OPTION 1 - THIS WILL REQUIRE CLICKING THE BUTTON
-function addCurrentAccounts() {
-    var accountBankValue = Number(document.getElementById('accountBankValue').value);
-    var accountSavingsValue = Number(document.getElementById('accountSavingsValue').value);
-
-    document.getElementById('totalAccountsValue').innerHTML = accountBankValue+accountSavingsValue
-    return false;
-}
-FOLLOWING STATEMENT ON LINE 14 IS INCOMPLETE - THIS IS JUST FOR REFERENCE
-document.getElementById('calcCosts').addEventListener('click', addCurrentAccounts);
-*/
-
-/*OPTION 2*/
-
+/* ------------------------------ CURRENT ACCOUNTS CALCULATION */
 document.getElementById('accountBankValue').addEventListener("input", totalAccountsValue);
 document.getElementById('accountSavingsValue').addEventListener("input", totalAccountsValue);
 
@@ -25,57 +14,15 @@ function totalAccountsValue() {
     $("input#totalAccountsValue").val(totalAccountsValue);
 }
 
+/* ------------------------------ INCOME CALCULATION */
 
+document.getElementById('netSalary').addEventListener("input", totalIncomeValue);
+document.getElementById('bonusSalary').addEventListener("input", totalIncomeValue);
+document.getElementById('gigSalary').addEventListener("input", totalIncomeValue);
 
-/*
-THE FOLLOWING IS FOR REFERENCE AND TAKEN FROM THE SOURCE CODE OF THE BUDGET CALCULATOR I FOUND ONLINE
-fnBC = function() {
-        var l = Number($("input#emergencyFund").val()) + Number($("input#investments").val()) + Number($("input#retirement").val());
-        l = (Math.round(l * 100) / 100).toFixed(2);
-        $("input#totalMonthlySavings").val(l);
-
-
-
-        var m = Number($("input#food").val()) + Number($("input#clothing").val()) + Number($("input#shelter").val()) + Number($("input#household").val()) + Number($("input#transportation").val()) + Number($("input#health").val()) + Number($("input#studentLoans").val()) + Number($("input#personal").val()) + Number($("input#misc").val());
-        m = (Math.round(m * 100) / 100).toFixed(2);
-        $("input#totalMonthlyExpenses").val(m);
-        var j = Number($("input#tuition").val()) + Number($("input#homeowners").val()) + Number($("input#car").val()) + Number($("input#taxes").val()) + Number($("input#vacation").val()) + Number($("input#other").val());
-        j = (Math.round(j * 100) / 100).toFixed(2);
-        $("input#totalAnnualExpenses").val(j);
-        var i = Number(j / 12);
-        i = (Math.round(i * 100) / 100).toFixed(2);
-        $("input#totalAnnualExpensesMonthly").val(i);
-        var n = Number($("input#monthlyPay").val()) + Number($("input#monthlyOther").val()) + Number($("input#annualGifts").val() / 12) + Number($("input#annualTaxReturn").val() / 12);
-        n = (Math.round(n * 100) / 100).toFixed(2);
-        $("input#totalMonthlyIncome").val(n);
-        var k = n - (Number(l) + Number(m) + (Number(j / 12)));
-        k = (Math.round(k * 100) / 100).toFixed(2);
-        $("input#underOverBudget").val(k)
-    }, 
-*/
-
-
-function addCosts() {
-    var rentMortageValue = Number(document.getElementById('rentMortageValue').value);
-    var energyValue = Number(document.getElementById('energyValue').value);
-    var waterValue = Number(document.getElementById('waterValue').value);
-    var insuranceValue = Number(document.getElementById('insuranceValue').value);
-    var mobileValue = Number(document.getElementById('mobileValue').value);
-    var streamingServicesValue = Number(document.getElementById('streamingServicesValue').value);
-    var donationsValue = Number(document.getElementById('donationsValue').value);
-
-    document.getElementById('totalCosts').innerHTML = rentMortageValue+energyValue+waterValue+insuranceValue+mobileValue+streamingServicesValue+donationsValue;
+function totalIncomeValue() {
+    var totalIncomeValue = Number($("input#netSalary").val()) + Number($("input#bonusSalary").val()) + Number($("input#gigSalary").val());
+    totalIncomeValue = (Math.round(totalIncomeValue * 100 / 100).toFixed(2));
+    $("input#totalIncomeValue").val(totalIncomeValue);
 }
 
-document.getElementById('calcCosts').addEventListener('click', addCosts);
-
-
-function subtract() {
-    var netSalary = Number(document.getElementById('netSalary').value);
-    var costs = Number(document.getElementById('totalCosts').value);
-
-    document.getElementById('result').innerHTML = netSalary-costs;
-    return false;
-}
-
-document.getElementById('calcCosts').addEventListener('click', subtract);
