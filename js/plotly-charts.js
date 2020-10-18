d462d1e975b5d82d582835b836d9fc0abcd80b64
Alexander Forrest <asforrest@icloud.com>
2020-10-18 15:35:04 +0000

    Updated plotly info

--- js/plotly-charts.js
+++ js/plotly-charts.js
@@ -1,43 +1,8 @@
-/* TESTING - CALCULATE INCOME PERCENTAGES */
-/*
-function getPercentageBankValue(){  
-    var accountBankValuePercentage=document.getElementById("accountBankValue").value;
-    var totalAccountsValuePercentage=document.getElementById("totalAccountsValue").value;
-    var A = (Math.floor((accountBankValuePercentage / totalAccountsValuePercentage) * 100)); 
-}
-
-function getPercentageSavingsValue(){  
-    var accountSavingsValuePercentage=document.getElementById("accountaccountSavingsValueBankValue").value;
-    var totalAccountsValuePercentage=document.getElementById("totalAccountsValue").value;
-    var B = (Math.floor((accountSavingsValuePercentage / totalAccountsValuePercentage) * 100)); 
-}
-
-
-function getPercentageCashValue(){  
-    var accountCashValuePercentage=document.getElementById("accountCashValue").value;
-    var totalAccountsValuePercentage=document.getElementById("totalAccountsValue").value;
-    var C = (Math.floor((accountCashValuePercentage / totalAccountsValuePercentage) * 100)); 
-}
-
 var data = [{
-  values: [12, 26, 55],
+  values: [50, 30, 20],
   labels: ['Bank Accounts', 'Savings Accounts', 'Cash-on-Hand'],
   type: 'pie'
 }];
-
-var contents = $("#accountCashValue");
-console.log(contents);
-
-
-
-
-function testingNumbers(){
-var number1 = 4.954848;
-var number2 = 5.9797;
-
-console.log(Math.floor((number1 / number2) * 100));
-}
-
 
 var layout = {
   height: 400,
@@ -46,4 +11,20 @@ var layout = {
 
 Plotly.newPlot('incomePieChart', data, layout);
 
-*/
+function testPER() {
+    var netSalaryPER =
+        Number($("input#netSalary").val()) / 
+        Number($("input#totalIncomeValue").val()) 
+        * 100;
+    var bonusSalaryPER =
+        Number($("input#bonusSalary").val()) / 
+        Number($("input#totalIncomeValue").val()) 
+        * 100;
+    var gigSalaryPER =
+        Number($("input#gigSalary").val()) / 
+        Number($("input#totalIncomeValue").val()) 
+        * 100;
+    data[0].values[0] = netSalaryPER;
+    data[0].values[1] = bonusSalaryPER
+    data[0].values[2] = gigSalaryPER
+}
