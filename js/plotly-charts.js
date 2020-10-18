99bbdae4c35f37a554d37907974da24d7440adee
Alexander Forrest <asforrest@icloud.com>
2020-10-18 13:48:53 +0000

    Attempting to get plotly to work

--- js/plotly-charts.js
+++ js/plotly-charts.js
@@ -1,36 +1,47 @@
-var data = [{
-  values: [19, 26, 55],
-  labels: ['Residential', 'Non-Residential', 'Utility'],
-  type: 'pie'
-}];
-
-var layout = {
-  height: 400,
-  width: 500
-};
-
-Plotly.newPlot('myDiv', data, layout);
-
-
-
-
 /* TESTING - CALCULATE INCOME PERCENTAGES */
 
 function getPercentageBankValue(){  
     var accountBankValuePercentage=document.getElementById("accountBankValue").value;
     var totalAccountsValuePercentage=document.getElementById("totalAccountsValue").value;
-alert(Math.floor((accountBankValuePercentage / totalAccountsValuePercentage) * 100)); 
+    var A = (Math.floor((accountBankValuePercentage / totalAccountsValuePercentage) * 100)); 
 }
 
 function getPercentageSavingsValue(){  
     var accountSavingsValuePercentage=document.getElementById("accountaccountSavingsValueBankValue").value;
     var totalAccountsValuePercentage=document.getElementById("totalAccountsValue").value;
-alert(Math.floor((accountSavingsValuePercentage / totalAccountsValuePercentage) * 100)); 
+    var B = (Math.floor((accountSavingsValuePercentage / totalAccountsValuePercentage) * 100)); 
 }
 
 
 function getPercentageCashValue(){  
     var accountCashValuePercentage=document.getElementById("accountCashValue").value;
     var totalAccountsValuePercentage=document.getElementById("totalAccountsValue").value;
-alert(Math.floor((accountCashValuePercentage / totalAccountsValuePercentage) * 100)); 
+    var C = (Math.floor((accountCashValuePercentage / totalAccountsValuePercentage) * 100)); 
 }
+
+var data = [{
+  values: [12, 26, 55],
+  labels: ['Bank Accounts', 'Savings Accounts', 'Cash-on-Hand'],
+  type: 'pie'
+}];
+
+var contents = $("#accountCashValue");
+console.log(contents);
+
+
+
+
+function testingNumbers(){
+var number1 = 4.954848;
+var number2 = 5.9797;
+
+console.log(Math.floor((number1 / number2) * 100));
+}
+
+
+var layout = {
+  height: 400,
+  width: 500
+};
+
+Plotly.newPlot('incomePieChart', data, layout);
