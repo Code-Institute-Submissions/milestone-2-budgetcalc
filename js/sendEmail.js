e89942ab672e6a0666141f440f6ad2ba7dc65f1c
Alexander Forrest <asforrest@icloud.com>
2020-10-19 05:15:02 +0000

    FEATURE: Added report a bug / request a feature option

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -40,3 +40,22 @@ function sendMail(contactForm) {
     );
     return false;  // To block from loading a new page
 }
+
+
+function sendBugMail(contactForm) {
+    emailjs.send("gmail", "resume-page", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "reportabug": contactForm.reportabug.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
